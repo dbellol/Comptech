ccda55277662a60f71eec27cef8e42cc597cffc4
steven_980213@hotmail.com <steven_980213@hotmail.com>
2022-05-07 13:15:07 -0500

    add get

---
+++ get-microservice/get-microservice/src/main/java/com/unal/firebase/management/models/grafica.java
@@ -0,0 +1,19 @@
+package com.unal.firebase.management.models;
+
+import lombok.Data;
+
+@Data
+public class grafica {
+    
+    private int anchoBanda;
+    private String arquitectura;
+    private int computo;
+    private int consumo;
+    private int frecuenciaBase;
+    private int frecuenciaOc;
+    private int id;
+    private String marca;
+    private int memoria;
+    private String nombre;
+    private int precio;
+}
