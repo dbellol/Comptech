b41da9e457089757353c571050dc10e533d819c4
steven_980213@hotmail.com <steven_980213@hotmail.com>
2022-05-07 13:14:54 -0500

    add get microservice

---
+++ get-microservice/get-microservice/src/main/java/com/unal/firebase/management/models/board.java
@@ -0,0 +1,18 @@
+package com.unal.firebase.management.models;
+
+import lombok.Data;
+
+@Data
+public class board {
+    
+    private int chipset;
+    private String compatibilidadArqRam;
+    private int id;
+    private String marca;
+    private int maxRam;
+    private String nombre;
+    private int precio;
+    private int puertos;
+    private String socket;
+    
+}
---
+++ get-microservice/get-microservice/src/main/java/com/unal/firebase/management/models/procesador.java
@@ -0,0 +1,19 @@
+package com.unal.firebase.management.models;
+
+import lombok.Data;
+
+@Data
+public class procesador {
+    private int cache;
+    private int consumo;
+    private int frecuencia;
+    private int generacion;
+    private Boolean graficaIntegrada;
+    private int hilos;
+    private int id;
+    private String marca;
+    private String nombre;
+    private int nucleos;
+    private int precio;
+    
+}
